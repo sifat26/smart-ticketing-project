d5efde505b99c176df4cc42dc805a94321c010ca
sifat26 <sifatict26@gmail.com>
2024-02-18 21:16:01 +0600

    functionality added

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let btnClick = 0;
+let totalSeat=0;
+let bookedSeat = 0;
+
 const buttons=document.getElementsByClassName("bton");
 for(let button of buttons){
     button.addEventListener("click",function(event){
@@ -9,12 +12,22 @@ for(let button of buttons){
             return;
             
         }
+
         colorChange(button);
 
     });
 }
 function colorChange(button){
     button.style.backgroundColor = "#1DD100";
+    const tSeat= document.getElementById('total-seat');
+    const totalSeat=parseInt(tSeat.innerText)-1;
+    tSeat.innerText=totalSeat;
+    const bSeat= document.getElementById('booked-seat');
+    const bookedSeat=parseInt(bSeat.innerText)+1;
+    bSeat.innerText=bookedSeat;
+
+
+
     
 
 
